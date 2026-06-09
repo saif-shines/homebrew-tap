e049f1eb8b0ede88c4be1fb4d8b37431f9753990
Saif Shines <saifshine7@gmail.com>
2026-06-09 20:03:35 +0530

    feat: initial doraval formula

---
+++ Formula/doraval.rb
@@ -0,0 +1,29 @@
+class Doraval < Formula
+  desc "The context engineering toolkit for coding agents"
+  homepage "https://github.com/saif-shines/doraval"
+  version "0.2.10"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/saif-shines/doraval/releases/download/v#{version}/doraval-darwin-arm64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    else
+      url "https://github.com/saif-shines/doraval/releases/download/v#{version}/doraval-darwin-x64"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "doraval-darwin-arm64" => "doraval"
+    else
+      bin.install "doraval-darwin-x64" => "doraval"
+    end
+    bin.install_symlink "doraval" => "dora"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/doraval --version")
+  end
+end
